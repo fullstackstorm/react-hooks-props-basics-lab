8a73b7b7a5201b739cabdc6f4730b4ea7fa3d4ff
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-05-23 20:12:30 -0700

    Pass the Links and Home tests.

---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Links(props) {
+    const {github, linkedin} = props;
+
+  return (
+    <>
+      <h3>Links</h3>
+      <a href={github}>{github}</a>
+      <a href={linkedin}>{linkedin}</a>
+    </>
+  );
+}
+
+export default Links;
